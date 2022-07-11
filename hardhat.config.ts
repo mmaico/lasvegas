173c1a01973cd59d571a317a990f915e8ae9c2c2
Marcelo Maico <mmaico@gmail.com>
2022-07-11 18:56:22 -0300

    Init project

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-gas-reporter";
+import "solidity-coverage"
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.15",
+
+
+};
+
+export default config;
